5f3411146af1f2a7c7cee41f4a6a9a8a73f53218
Gstars01 <khw4986@gmail.com>
2025-12-08 02:37:11 +0900

    change IntusionDetector structure change instruction of class IntrusionDetector.py

--- IntrusionDetector.py
+++ IntrusionDetector.py
@@ -1,14 +1,43 @@
 import cv2
+from Camera import Camera
 from Event import Event
+
+#모션 감지 캐퍼시터
+motion_cap = []
 
 class IntrusionDetector:
     def __init__(self, camera):
         self.camera = camera
         self.fgbg = cv2.createBackgroundSubtractorMOG2(
-            history=40,
-            varThreshold=16,
-            detectShadows=False
+            history=40,  #배경 모델 학습할 프레임수를 40으로 정함
+            varThreshold=16, #픽셀 색상 변경 허용범위
+            detectShadows=False #그림자 감지 여부
         )
+
+    #감지 및 카운트
+    def run(self):
+        while True:
+            frame = self.camera.capture_frame()
+            if frame is None:
+                break
+
+            # 움직임 감지
+            motion_detected = self.detect_motion(frame)
+            if motion_detected:
+                motion_cap.append(motion_detected)
+                #10프레임 이상 감지시 True 리턴
+                if len(motion_cap)>10:
+                    return True
+            print(motion_detected)
+            print(motion_cap)
+
+            # 프레임 출력
+            cv2.imshow("Frame", frame)
+
+            if cv2.waitKey(17) == 27:  # ESC
+                break
+
+        cv2.destroyAllWindows()
 
     def detect_motion(self, frame):
         # 배경 차분
@@ -26,6 +55,3 @@ class IntrusionDetector:
             if cv2.contourArea(cnt) >= 300:  # 일정 크기 이상 움직임이면 True
                 return True
         return False  # 아무것도 없으면 False
-
-    def create_intrusion_event(self,type,loaction):
-        return Event(self,type,loaction)
--- Test_2.py
+++ Test_2.py
@@ -1,23 +1,8 @@
-import cv2
 from Camera import Camera
 from IntrusionDetector import IntrusionDetector
 
 camera = Camera('dummy.mp4')
 intrusion = IntrusionDetector(camera)
 
-while True:
-    frame = camera.capture_frame()
-    if frame is None:
-        break
-
-    # 움직임 감지 (True/False)
-    motion_detected = intrusion.detect_motion(frame)
-
-    print(motion_detected)
-    # 프레임 출력
-    cv2.imshow("Frame", frame)
-
-    if cv2.waitKey(17) == 27:  # ESC
-        break
-
-cv2.destroyAllWindows()
+# 객체 생성 후 바로 실행
+print("결과값 : ",intrusion.run())
