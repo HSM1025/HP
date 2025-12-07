093c316c48f0cbd84df60d75cf9db5249ba0ad58
Gstars01 <khw4986@gmail.com>
2025-12-08 07:15:38 +0900

    self

--- IntrusionDetector.py
+++ IntrusionDetector.py
@@ -51,19 +51,19 @@ class IntrusionDetector:
                     # 모션이 찾아진 카메라 배열
                     self.intrusion_flags[idx] = True
                     if idx==0 and self.callcount0==0:
-                        analyze = __aiAnalyzer.analyze(frame,"INTRUDER")
+                        analyze = self.__aiAnalyzer.analyze(frame,"INTRUDER")
                         if analyze:
                             create_Intrusion_Event("Intrusion", "camera1")
                             self.callcount0 += 1
 
                     elif idx==1 and self.callcount1==0:
-                        analyze = __aiAnalyzer.analyze(frame, "INTRUDER")
+                        analyze = self.__aiAnalyzer.analyze(frame, "INTRUDER")
                         if analyze:
                             create_Intrusion_Event("Intrusion", "camera2")
                             self.callcount1 += 1
 
                     elif idx==2 and self.callcount2==0:
-                        analyze = __aiAnalyzer.analyze(frame, "INTRUDER")
+                        analyze = self.__aiAnalyzer.analyze(frame, "INTRUDER")
                         if analyze:
                             create_Intrusion_Event("Intrusion", "camera3")
                             self.callcount2 += 1
