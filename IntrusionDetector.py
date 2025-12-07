261454d1b28fbc22d19f107ced715374bc504588
Gstars01 <khw4986@gmail.com>
2025-12-08 03:28:23 +0900

    change IntrusionDetector.py - change motion_cap position global -> self - add value intrusion_over_10 : Boolean - change main structure of making return value   ㄴ> throgh add value intrusion_over_10

--- IntrusionDetector.py
+++ IntrusionDetector.py
@@ -2,38 +2,29 @@ import cv2
 from Camera import Camera
 from Event import Event
 
-#모션 감지 캐퍼시터
-motion_cap = []
-
 class IntrusionDetector:
     def __init__(self, camera):
         self.camera = camera
+        self.motion_cap = []
+        self.intrusion_over_10 = False
         self.fgbg = cv2.createBackgroundSubtractorMOG2(
-            history=40,  #배경 모델 학습할 프레임수를 40으로 정함
-            varThreshold=16, #픽셀 색상 변경 허용범위
-            detectShadows=False #그림자 감지 여부
+            history=40,
+            varThreshold=16,
+            detectShadows=False
         )
 
-    #감지 및 카운트
-    def run(self):
         while True:
             frame = self.camera.capture_frame()
             if frame is None:
                 break
 
-            # 움직임 감지
             motion_detected = self.detect_motion(frame)
-            if motion_detected:
-                motion_cap.append(motion_detected)
-                #10프레임 이상 감지시 True 리턴
-                if len(motion_cap)>10:
-                    return True
             print(motion_detected)
-            print(motion_cap)
-
+            if len(self.motion_cap) > 10 :
+                self.intrusion_over_10 = True
+                break
             # 프레임 출력
             cv2.imshow("Frame", frame)
-
             if cv2.waitKey(17) == 27:  # ESC
                 break
 
@@ -49,9 +40,10 @@ class IntrusionDetector:
 
         # 윤곽선 찾기
         contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+        print(self.motion_cap)
         # 움직임 있는지 판정
         for cnt in contours:
-            if cv2.contourArea(cnt) >= 300:  # 일정 크기 이상 움직임이면 True
-                return True
-        return False  # 아무것도 없으면 False
+            if cv2.contourArea(cnt) >= 300:
+                self.motion_cap.append(True)
+
+        return False
--- Test_2.py
+++ Test_2.py
@@ -5,4 +5,4 @@ camera = Camera('dummy.mp4')
 intrusion = IntrusionDetector(camera)
 
 # 객체 생성 후 바로 실행
-print("결과값 : ",intrusion.run())
+print("결과값 : ",intrusion.intrusion_over_10)
