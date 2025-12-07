df1ccc30e43e783f72e255c18c76c6c2edc6306e
HSM1025 <gkatjdah070@gmail.com>
2025-12-08 06:38:07 +0900

    updata FireDetector and Singleton

--- EventManager.py
+++ EventManager.py
@@ -1,4 +1,18 @@
-class EventManager:
+class Singleton:
+    __instance = None
+
+    @classmethod
+    def __get_instance(cls):
+        return cls.__instance
+
+    @classmethod
+    def instance(cls, *args, **kwargs):
+        cls.__instance = cls(*args, **kwargs)
+        cls.instance = cls.__get_instance
+        return cls.__instance
+
+
+class EventManager(Singleton):
     def __init__(self):
         self.__eventQueue = []
 
